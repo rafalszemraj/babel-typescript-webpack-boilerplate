8a6e71cee8dbeaae4264e8b5546d53773a9b46ea
Rafał Szemraj (XRAFS) <xrafs@saxobank.com>
2019-11-06 09:58:09 +0100

    Added fallback to default Removed [app] from imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import ticket from 'src/modules/ticket/ticket'
-import config from 'src/app/[app]/moduleConfig'
+import ticket from 'src/modules/ticket/ticket' // ST: 'src/modules/ticket/ticket.[app]
+import universal from 'src/modules/universal/universal' // ST: 'src/modules/universal/universal
+import config from 'src/app/moduleConfig' // ST: src/app/[app]/moduleConfig
 
 config()
 ticket()
+universal()
